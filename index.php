1c75d227c094433e8671525f2553ef96f5b5517f
vishal-pandey <pandeyvishal25@yahoo.com>
2017-09-04 23:10:47 +0530

    first commit

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <link href="https://fonts.googleapis.com/css?family=Anton|PT+Sans+Narrow" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 	<section class="main col-sm-12">
