abad42a434e6400f31d40795a2429b0fdd26d2d7
vishal-pandey <pandeyvishal25@yahoo.com>
2017-09-04 23:06:26 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Programming Club</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Anton|PT+Sans+Narrow" rel="stylesheet">
+</head>
+<body>
+	<section class="main col-sm-12">
+		<section class="heading"><h1>Welcome to programming competition</h1></section>
+		<section class="competition-container col-sm-9">
+			<section class="competition col-sm-4 well">
+				<div class="poster"><img src="./2.jpg" class="img-circle"></div>
+				<div class="info">
+					<h3 align="center">Code Challenge And War III</h3>
+					<center>
+						<b>Venue : </b><span>SOICT</span><br />
+						<b>Date : </b><span>11/09/2017</span><br />
+						<b>Description : </b><span>Individual Competitive Coding<br /><br /><button class="btn btn-default" id="c1">Learn More</button></span><br />
+					</center>
+				</div>
+			</section>
+			<section class="competition col-sm-4 well">
+				<div class="poster"><img src="./2.jpg" class="img-circle"></div>
+				<div class="info">
+					<h3 align="center">Model And Idea Showcase</h3>
+					<center>
+						<b>Venue : </b><span>SOICT</span><br />
+						<b>Date : </b><span>Submit Online by 13/09/2017</span><br />
+						<b>Description : </b><span>Technical Idea / Model Presentation<br /><br /><button class="btn btn-default" id="c2">Learn More</button></span><br />
+					</center>
+				</div>
+			</section>
+			<section class="competition col-sm-4 well">
+				<div class="poster"><img src="./2.jpg" class="img-circle"></div>
+				<div class="info">
+					<h3 align="center">Make-athon<br>&nbsp;</h3>
+					<center>
+						<b>Venue : </b><span>SOICT</span><br />
+						<b>Date : </b><span>Submit Online by 13/09/2017</span><br />
+						<b>Description : </b><span>Android Development and Web Developement Product Showcase<br /><br /><button class="btn btn-default" id="c3">Learn More</button></span><br />
+					</center>
+				</div>
+			</section>
+			<section class="competition Register col-sm-12 well">
+				<div class="info">
+					<h3 align="center"><a href="https://docs.google.com/forms/d/e/1FAIpQLSeqkjQxUgN_7Cj_QhH8CaabFTnoqFNvUaR2OmGtRltg4RIfGw/viewform?c=0&w=1"><button class="btn btn-success">Register For Competition</button></a></h3>
+				</div>
+			</section>
+		</section>
+	</section>
+
+
+	<section class="desc c1 col-sm-12">
+		
+		<div class="col-sm-9 well long-desc">
+			<span class="close-c"><button class="btn btn-default close" style="color: black"><span class="glyphicon glyphicon-remove-sign"></span></button></span>
+		<h1 align="center">Name Of Competition</h1>
+		<div class="poster1"><img src="./2.jpg" class="img-rounded"></div>
+			<h3 align="center">Its an Algorithm Based Coding Contest.</h3>
+			<b>12th November :</b> Junior<br>
+
+			<b>Scope :</b> For First and Second year students<br><br>
+
+
+
+			<b>12th November :</b> Senior<br>
+
+			<b>Scope :</b> For Third, Fourth and Fifth year students + Top  10 participants from Junior group<br>
+
+			<b>Time :</b> 1: PM onwards<br>
+
+			<b>Type :</b> Individual<br>
+
+
+
+			<h3 align="center">RULES FOR ALGORITHM BASED CODING CONTEST</h3>
+
+			<b>Rule 1 :</b> Individual participation only<br><br>
+
+			<b>Rule 2 :</b> The participants will be divided into two groups<br><br>
+
+            <b>GROUP A :</b> 1st and 2nd year students - will be held on 12th Nov<br><br>
+
+            <b>GROUP B :</b> 3rd, 4th, 5th year students - will be held on 12th Nov<br><br>
+
+			<b>Rule 3 :</b> The participants can use any programming language<br><br>
+
+			<b>Rule 4 :</b> There will be five questions for each group<br><br>
+
+			<b>Rule 5 :</b> The participant may bring their own laptop, If they don’t have the laptop, a system will be provided to them<br><br>
+
+			<b>Rule 6 :</b> Internet connectivity is required to conduct the competition<br><br>
+
+			<b>Rule 7 :</b> The prizes will be given to the top three winners of each group<br><br>
+
+			<b>Rule 8 :</b> The competition will be similar to the programming challenges which are regularly organized on Codechef, HackerEarth, etc<br><br>
+
+			<b>Rule 9 :</b> GP will be given to all participants<br><br>
+
+			<b>Rule 10 :</b> Top10 participants of group A can take part in group B competition<br><br>
+
+
+
+			<b>Venue :</b> SoICT (IP-102)<br><br>
+
+			<b>Registration link :</b>
+
+			<a href="https://docs.google.com/forms/d/e/1FAIpQLSeqkjQxUgN_7Cj_QhH8CaabFTnoqFNvUaR2OmGtRltg4RIfGw/viewform?c=0&w=1">Register</a><br>
+		</div>
+	</section>
+	
+</body>
+</html>
+<style type="text/css">
+	body{
+		margin: 0;
+		padding: 0;
+	}
+	.main{
+		height: 100vh;
+		width: 100%;
+		background-image: url("1.jpeg");
+		background-repeat: no-repeat;
+		background-size: 100% 100vh;
+		background-attachment: fixed;
+		overflow: auto;
+		position: absolute;
+	}
+
+	.competition{
+		background: rgba(111,111,111,0.5);
+		color: white;
+
+		margin-top: 100px;
+	}
+	.heading h1{
+		color: white;
+		text-align: center;
+		text-shadow: 0px 0px 30px white;
+		font-family: 'Anton', sans-serif;
+		font-size: 55px;
+	}
+	
+	.poster img{
+		height: 150px;
+		width: 150px;
+		margin-top: -100px;
+	}
+	.poster, .poster1{
+		text-align: center;
+	}
+	.competition-container{
+		float: initial;
+		margin-left: auto;
+		margin-right: auto;
+		margin-top: 30px;
+	}
+	.Register{
+		margin-top: 30px;
+	}
+
+	/*descrition page styling*/
+	.desc{
+		width: 100%;
+		height: 100vh;
+		position: absolute;
+		background-image: url("https://warroom.securestate.com/wp-content/uploads/2016/10/coding.jpg");
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: auto 100vh;
+		background-attachment: fixed;
+		display: none;
+		overflow: auto;
+	}
+	.close{
+		font-size: 25px;
+		margin-top: 5px;
+	}
+	.poster1 img{
+		height: auto;
+		width: 50%;
+	}
+	.long-desc{
+		margin-left: auto;
+		margin-right: auto;
+		float: initial;
+		background: rgba(238,238,238,0.9);
+	}
+	.close-c{
+		background-color: white;
+		width: 10px;
+		height: 10px;
+	}
+
+	@media screen and (max-width: 786px) {
+		.desc{
+			padding: 0;
+		}
+
+	}
+
+
+
+</style>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("#c1").click(function(){
+			$(".c1").css("display","block");
+		});
+		$(".close").click(function(){
+			$(".c1").css("display","none");
+		});
+		$("#c2").click(function(){
+			$(".c1").css("display","block");
+		});
+		$(".close").click(function(){
+			$(".c1").css("display","none");
+		});
+		$("#c3").click(function(){
+			$(".c1").css("display","block");
+		});
+		$(".close").click(function(){
+			$(".c1").css("display","none");
+		});
+
+	});
+</script>
